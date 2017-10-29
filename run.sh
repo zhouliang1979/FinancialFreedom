c655e7e981a2ffc822415d4a1eeac288d7705ed6
孝文 <xiaowen.zl@taobao.com>
2017-10-29 19:38:42 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-python demo.py --category rb1801 --start 20170823 --end 20170901 --barsize 1800 --ma  35 --bandwidth 20 --tickprice 1
-#python test_case.py --category rb1801 --start 20170823 --end 20170901 --tickprice 1
+rm -f nohup.out
+#python demo.py --category rb1801 --start 20170823 --end 20170901 --barsize 1800 --ma  35 --bandwidth 20 --tickprice 1
+nohup python test_case.py --category rb1801 --start 20170823 --end 20170901 --tickprice 1  &
+
